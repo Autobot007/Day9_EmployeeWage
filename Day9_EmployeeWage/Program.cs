5fa9b217ded0ec939be6c44f64a47235992375bd
Sarfaraaz Shaikh <s0rf0r00z50@gmail.com>
2022-08-16 17:00:45 +0530

    [SARFARAAZ]- UC5 Wages for Month Implemented

--- Day9_EmployeeWage/Program.cs
+++ Day9_EmployeeWage/Program.cs
@@ -12,24 +12,34 @@
             int fullDayHour = 8;
             int partTimeHour = 4;
             int wagePerHour = 20;
+            int workingHour = 0;
+            int day = 0;
 
 
             EmployeeWage Employee = new();
-
-            switch (Employee.IsPresent())
+            for (int i = 1; i <= 20; i++)
             {
-                case 0:
-                    Console.WriteLine("Employee is Absent");
-                    break;
-                case 1:
-                    Console.WriteLine("Employee is Present for Part time");
-                    Console.WriteLine("Employees todays wage is : "+Employee.DailyWage(partTimeHour,wagePerHour));
-                    break;
-                case 2:
-                    Console.WriteLine("Employee is Present");
-                    Console.WriteLine("Employees todays wage is : " + Employee.DailyWage(fullDayHour, wagePerHour));
-                    break;
+                switch (Employee.IsPresent())
+                {
+                    case 0:
+                        Console.WriteLine(day+"  Employee is Absent\n");
+                       day++;i--;
+                        break;
+                    case 1:
+                        Console.WriteLine(day+"  Employee is Present for Part time");
+                        Console.WriteLine("Employees todays wage is : " + Employee.DailyWage(partTimeHour, wagePerHour)+"\n");
+                        workingHour += 4; day++;
+                        break;
+                    case 2:
+                        Console.WriteLine(day+"  Employee is Present");
+                        Console.WriteLine("Employees todays wage is : " + Employee.DailyWage(fullDayHour, wagePerHour)+"\n");
+                        workingHour += 8; day++;
+                        break;
+                }
+
             }
+
+            Console.WriteLine("Monthly Wage is :" +workingHour*wagePerHour);
                 
 
 
