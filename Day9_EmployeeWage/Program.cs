9708e98bbfc8a53fa95cd06d39c9af36df87a430
Sarfaraaz Shaikh <s0rf0r00z50@gmail.com>
2022-08-16 14:21:44 +0530

    [SARFARAAZ]-Welcome to Employee Wage Computation

---
+++ Day9_EmployeeWage/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day9_EmployeeWage
+{
+    public class Program
+    {
+        public static  void Main(string[] args)
+        {
+            Console.WriteLine("Welcome o Employee Wage Computation Problem");
+        }
+    }
+}
